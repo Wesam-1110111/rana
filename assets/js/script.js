9f8c6fb8aa3b0f57154e097e135f5cf1e6bd00c2
Mr. 1110111 <108031419+Wesam-1110111@users.noreply.github.com>
2025-07-01 18:34:03 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,21 @@ window.addEventListener("scroll", function () {
       navbar.classList.remove("scrolled");
     }
   });
-  // نص متغير بتأثير سلس
-    const words = ["تصميم إعلاني؟", "هوية بصرية؟", "حملة ممولة؟", "موقع إلكتروني؟", "تصوير إحترافي؟", "كتابة محتوى؟"];
-    let wordIndex = 0;
-  const wordEl = document.getElementById("changing-word");
 
-  function rotateWord() {
-    wordEl.classList.remove("show");
-    setTimeout(() => {
-      wordEl.textContent = words[wordIndex];
-      wordEl.classList.add("show");
-      wordIndex = (wordIndex + 1) % words.length;
-    }, 300);
-  }
+  // // نص متغير بتأثير سلس
+  //   const words = ["تصميم إعلاني؟", "هوية بصرية؟", "حملة ممولة؟", "موقع إلكتروني؟", "تصوير إحترافي؟", "كتابة محتوى؟"];
+  //   let wordIndex = 0;
+  // const wordEl = document.getElementById("changing-word");
 
-  rotateWord();
-  setInterval(rotateWord, 2000); // كل كلمتين في 2 ثانية (تعديل بسيط)
+  // function rotateWord() {
+  //   wordEl.classList.remove("show");
+  //   setTimeout(() => {
+  //     wordEl.textContent = words[wordIndex];
+  //     wordEl.classList.add("show");
+  //     wordIndex = (wordIndex + 1) % words.length;
+  //   }, 300);
+  // }
+
+  // rotateWord();
+  // setInterval(rotateWord, 2000); // كل كلمتين في 2 ثانية (تعديل بسيط)
   
